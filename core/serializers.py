79c3cb5bcffb074e34a7e6b6e1617fa7ce5e1859
Azhybek Nurlanov <https://github.com/Azhybek/exem_python.git>
2021-07-31 16:51:27 +0600

    add Serializer

---
+++ core/serializers.py
@@ -0,0 +1,9 @@
+from django.db import models
+from rest_framework import serializers
+from .models import Proposal
+
+
+class ProposalListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Proposal
+        fields = ['id', 'title']
